56c8473aed67bd590e0cd6e1f7c05958d6b46897
Getinet Amare <getinet.dtu@outlook.com>
2021-08-26 10:27:47 +0300

    Adding 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 /**
  * Use the function printf
  * You are not allowed to use the function puts
@@ -7,6 +7,6 @@
 */
 int main (void)
 {
-	printf ("\"with proper grammar, but the outcome is a piece of art,");
+	printf ("%s","with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
