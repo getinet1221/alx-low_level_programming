0a510592d614c2d0df592b70c4f7beeef0f2fc65
Getinet Amare <getinet.dtu@outlook.com>
2021-09-01 13:48:20 +0300

    Adding a file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,18 +13,3 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
