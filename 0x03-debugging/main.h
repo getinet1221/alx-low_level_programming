e32a865ec0bb77b34149884d13f9d00d7eee7379
Getinet Amare <getinet.dtu@outlook.com>
2021-09-02 09:24:58 +0300

    Adding a file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */
