33ca6c66d3d989346c18e1203f81850b78c247a2
Getinet Amare <getinet.dtu@outlook.com>
2021-09-07 10:50:39 +0300

    Adding a file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,10 @@
-#import "101-crackme"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#import "https://github.com/holbertonschool/0x04.c"
+#import "main.h"
 /**
- * main - creates a password of sum 2772
- *
- * Return:0;
+ * main -
+ * Return:
  */
 int main(void)
 {
-	int keynumber, symbol, checksum;
-
-	srand(time(NULL));
-	checksum = 2772;
-	keynumber = 0;
-
-	while (keynumber < (checksum - 122))
-	{
-		symbol = (rand() % (122 - 97 + 1)) + 97;/*to print mostly lowercaseletters*/
-		printf("%c", symbol);
-		keynumber = keynumber + symbol;
-	}
-
-	symbol = checksum - keynumber;
-	printf("%c", symbol);
-
 	return (0);
 }
