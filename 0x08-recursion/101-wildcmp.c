dfa08fe43cec4a47ae138183c781ff780e0b4c60
Getinet Amare <getinet.dtu@outlook.com>
2021-09-15 13:21:51 +0300

    Adding a file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,42 @@
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int _putchar.c(char c);
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
+
+/**
+ *
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	len = _strlen_recursion(s1);
+
+	return (tmp(s1, s2, len));
+}
+
+/**
+ *
+ */
+
+int tmp(char *s1, char *s2, len)
+{
+	if (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 == *s2)
+			return (tmp(s1 + 1, s2 +  1));
+		else
+			return (0);
+	}
+	return (1)
+}
