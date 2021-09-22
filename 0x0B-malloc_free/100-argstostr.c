0b74b611ace56bc168403ea21a94f660cc07d4fc
Getinet Amare <getinet.dtu@outlook.com>
2021-09-22 13:30:15 +0300

    Changing  a file:

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,37 +1,86 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * argstostr - concatenates all arguments of program.
- * @ac: argument count
- * @av: pointer to argument vectors
- * Return: pointer to new buffer with all arguments as 1 string.
+ * wordcounter - counts words and the letters in them
+ * @str: string to count
+ * @pos: position of the word to count characters from
+ * @firstchar: position of the first letter of the word
+ * if pos = 0, count the number of chars in the word
+ * else count number of words
+ * Return: wordcount if pos == 0,
+ * length of word if pos > 0,
+ * position of word if pos > 0 && firstchar > 0
  */
-char *argstostr(int ac, char **av)
+int wordcounter(char *str, int pos, char firstchar)
 {
-	int i, j, len, bufferlen;
-	char *p;
+	int i, wordcount, charcount, flag;
 
-	if (ac == 0)
-		return (NULL);
-	if (av == NULL)
-		return (NULL);
-
-	i = j = len = bufferlen = 0;
-	for (i = 0; av[i]; i++)
+	str[0] != ' ' ? (wordcount = 1) : (wordcount = 0);
+	for (i = 0, flag = 0; str[i]; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			len++;
+		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' && flag == 0)
+		{
+			wordcount++;
+			flag = 1;
+		}
+		if (pos > 0 && pos == wordcount)
+		{
+			if (pos > 0 && pos == wordcount && firstchar > 0)
+				return (i);
+			for (charcount = 0; str[i + charcount + 1] != ' '; charcount++)
+				;
+			return (charcount);
+		}
+		if (str[i] == ' ')
+			flag = 0;
 	}
-	p = (char *)malloc(len * sizeof(char) + ac + 1);
+	return (wordcount);
+}
+/**
+ * strtow - convert a string into a 2d array of words
+ * @str: string to convert
+ * Return: double pointer to 2d array
+ */
+char **strtow(char *str)
+{
+	int wc, wordlen, getfirstchar, len, i, j;
+	char **p;
+
+	for (len = 0; str[len]; len++)
+		;
+	if (str == NULL)
+		return (NULL);
+	wc = wordcounter(str, 0, 0);
+	if (len == 0 || wc == 0)
+		return (NULL);
+	p = malloc((wc + 1) * sizeof(void *));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; av[i]; i++)
+	for (i = 0, wordlen = 0; i < wc; i++)
 	{
-		for (j = 0; av[i][j]; j++, bufferlen++)
-			p[bufferlen] = av[i][j];
-		p[bufferlen] = '\n';
-		bufferlen++;
+		/* Allocate memory for nested elements */
+		wordlen = wordcounter(str, i + 1, 0);
+		if (i == 0 && str[i] != ' ')
+			wordlen++;
+		p[i] = malloc(wordlen * sizeof(char) + 1);
+		if (p[i] == NULL)
+		{
+			for ( ; i >= 0; --i)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		/* initialize each element of the nested array with the word*/
+		getfirstchar = wordcounter(str, i + 1, 1);
+		if (str[0] != ' ' && i > 0)
+			getfirstchar++;
+		else if (str[0] == ' ')
+			getfirstchar++;
+		for (j = 0; j < wordlen; j++)
+			p[i][j] = str[getfirstchar + j];
+		p[i][j] = '\0';
 	}
-	p[bufferlen] = '\0';
+	p[i] = NULL;
 	return (p);
 }
