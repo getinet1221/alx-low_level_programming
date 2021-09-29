70a15d216729592cb6f0bc1cae27a9999988ad25
Getinet Amare <getinet.dtu@outlook.com>
2021-09-29 17:56:37 +0300

    Adding a file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,115 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * string_nconcat - concatenate two strings up to n bytes.
- * @s1: source string
- * @s2: string to truncate up to n bytes
- * @n: number of bytes to truncate by
- * Return: pointer to new buffer
+ * _strlen - Counts the lenght of the string
+ * @s: String to analyze
+ *
+ * Return: Lenght of the string
  */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
+
+
+/**
+ * _strcpy - Copies the string from a source to a destination
+ * @dest: Where to copy the string
+ * @src: String to copy
+ *
+ * Return: dest's pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	count = 0;
+
+	while (*src != '\0')
+	{
+		*(dest + count) = *src;
+		src++;
+		count++;
+	}
+	*(dest + count) = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * _strncat - Concatenates a string up to the n byte
+ * @dest: String to concatenate
+ * @src: String used to concatenate
+ * @n: Amount of bytes to concatenate
+ *
+ * Return: Destination string (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count_dest;
+	int count_src;
+
+	count_dest = 0;
+	count_src = 0;
+
+	while (*(dest + count_dest) != '\0')
+	{
+		count_dest++;
+	}
+
+	while (*(src + count_src) != '\0' && count_src < n)
+	{
+		*(dest + count_dest) = *(src + count_src);
+		count_dest++;
+		count_src++;
+	}
+
+	*(dest + count_dest) = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * string_nconcat - Concatenates n bytes of one string into another string
+ * @s1: Destination string (string to concatenate)
+ * @s2: Source string (string to add)
+ * @n: Amount of bytes from s2 to concatenate to s1
+ * Return: Pointer to the concatenated string or NULL on failure
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int s1count, s2count, sizeBuffer, i;
+	char *out;
+	unsigned int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	s2count > n ? (s2count = n) : (n = s2count);
-	sizeBuffer = s1count + s2count + 1;
-	p = malloc(sizeBuffer * sizeof(char));
-	if (p == NULL)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n >= len2)
+		out = malloc(sizeof(char) * (len1 + len2 + 1));
+	else
+		out = malloc(sizeof(char) * (len1 + n + 1));
+	if (out == NULL)
 		return (NULL);
-	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
-	return (p);
+
+	out = _strcpy(out, s1);
+	out = _strncat(out, s2, n);
+	return (out);
 }
