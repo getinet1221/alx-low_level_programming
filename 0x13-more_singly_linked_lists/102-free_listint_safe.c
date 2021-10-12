b4d6969197e8ac58b42f9a73a16479f93cf8dc4d
Getinet Amare <getinet.dtu@outlook.com>
2021-10-12 12:09:56 +0300

    Adding a file

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,32 @@
 #include "lists.h"
+
 /**
-  * free_listint_safe - define function
-  * @h:
-  * Return: what does it return?
-  */
+ * free_listint_safe - frees a list
+ * @h: the pointer to the start of list to free
+ * Return: size of free'd list (in nodes?)
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	size_t nodeCount = 0;
+	listint_t *temp = NULL;
+
+	if (!(h && *h))
+		return (nodeCount);
+	while (*h)
+	{
+		nodeCount++;
+		if (*h > (*h)->next)
+		{
+			temp = *h;
+			*h = (*h)->next;
+			free(temp);
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+		}
+	}
+	*h = NULL;
+	return (nodeCount);
 }
